4ef6e63fe9c4c34fd4c9169b31efd3c669b51173
Leonardo Firmino da Silva <leofirsil@outlook.com>
2025-04-09 23:12:09 -0300

    trocando a estilização para o Styled Components

---
+++ src/components/Cabecalho/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import cores from '../../cores'
+
+export const Header = styled.header`
+  background-color: ${cores.corSecundaria};
+  color: ${cores.corPrincipal};
+  text-align: center;
+  padding: 24px 0;
+`
